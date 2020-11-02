457a27c7ac2d854516e500eea7c9f7270bc5aeea
jenthone <jenthone@gmail.com>
2020-11-02 21:56:05 +0700

    Delete data folder in presentation layer

--- presentation/lib/data/api.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-
-const _baseUrl = "https://hacker-news.firebaseio.com/v0/";
-
-final _options = BaseOptions(
-  baseUrl: _baseUrl,
-);
-final dio = Dio(_options);
--- presentation/lib/data/repository/story_repository_impl.dart
+++
@@ -1,9 +0,0 @@
-import 'package:domain/entity/story_type.dart';
-import 'package:domain/repository/story_repository.dart';
-
-class StoryRepositoryImpl implements StoryRepository {
-  @override
-  Future<List<int>> fetchStories(StoryType type) {
-    throw UnimplementedError();
-  }
-}

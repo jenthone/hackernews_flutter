91d8e33f3da69eb50f7004cccdf7792ea881b3cc
jenthone <jenthone@gmail.com>
2020-11-08 20:07:12 +0700

    Update dependencies

--- presentation/lib/injection.dart
+++ presentation/lib/injection.dart
@@ -1,15 +1,20 @@
+import 'package:data/api.dart';
+import 'package:data/repository/item_repository_impl.dart';
+import 'package:data/service/item_service.dart';
 import 'package:get_it/get_it.dart';
+
+import 'package:domain/repository/item_repository.dart';
 
 final locator = GetIt.instance;
 
 void configureDependencies() async {
-  // locator.registerSingleton(dio);
+  locator.registerSingleton(dio);
 
-  // locator.registerFactory(() => ItemService(locator.get()));
+  locator.registerFactory(() => ItemService(locator.get()));
 
-  // locator.registerFactory<ItemRepository>(
-  //   () => ItemRepositoryImpl(
-  //     locator.get(),
-  //   ),
-  // );
+  locator.registerFactory<ItemRepository>(
+    () => ItemRepositoryImpl(
+      locator.get(),
+    ),
+  );
 }
